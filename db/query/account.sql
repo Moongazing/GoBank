2c0fc4a6d0a65f6644af4997bb407ad7a1d73e23
Moongazing <tunahan.ali.ozturk@outlook.com>
2025-03-14 16:29:04 +0100

    add SQL queries for account management and update generated code header

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,28 @@ INSERT INTO accounts (
   $1, $2, $3
 ) RETURNING *;
 
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+SELECT * FROM accounts
+WHERE owner = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
+DELETE FROM accounts
+WHERE id = $1;
